098e2b2d7426451a8d85a52a17fdc10bd9319a3c
russgbiv <rgboyd10@gmail.com>
2018-11-28 16:58:30 -0600

    -

--- include/Orbital_Positions.cpp
+++ include/Orbital_Positions.cpp
@@ -1,12 +1,13 @@
 // For compilers that don't support precompilation, include "wx/wx.h"
 #include "wx/wxprec.h"
- 
 #ifndef WX_PRECOMP
-#	include "wx/wx.h"
+#include "wx/wx.h"
 #endif
- 
+
+#include <string>
 #include "Orbital_Positions.h"
- 
+using namespace std;
+
 IMPLEMENT_APP(Orbital_Positions)
  
 // This is executed upon startup, like 'main()' in non-wxWidgets programs.
@@ -18,5 +19,10 @@ bool Orbital_Positions::OnInit()
 	frame->SetStatusText(_T("Hello World"));
 	frame->Show(true);
 	SetTopWindow(frame);
+	wxMenu* menus[3];
+	wxString titles[] = new wxString[3];
+	wxMenuBar *mbar = new wxMenuBar(1, menus, titles[], 0);
+	mbar->Insert(0,menus,"Test1");
+		
 	return true;
-}
+
--- include/Orbital_Positions.h
+++ include/Orbital_Positions.h
@@ -1,6 +1,6 @@
 #ifndef INCLUDED_ORBITAL_POSITIONS_H
 #define INCLUDED_ORBITAL_POSITIONS_H
- 
+#include <string>
 class Orbital_Positions : public wxApp
 {
  public:
